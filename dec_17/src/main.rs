573a9dd5c629fec1264edc9265dad1d76b663a59
Adam Barla <barlaada@fit.cvut.cz>
2024-12-18 00:31:26 +0100

    feat: day 17 **

--- dec_17/src/main.rs
+++ dec_17/src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-fn combo(rs: &mut Vec<i32>, o: i32) -> i32 {
+fn c(rs: &[u64; 3], o: u64) -> u64 {
     match o {
         0..=3 => {o}
         4..=6 => {rs[(o-4) as usize]}
@@ -11,20 +12,23 @@ fn combo(rs: &mut Vec<i32>, o: i32) -> i32 {
     }
 }
 
-fn instr(rs: &mut Vec<i32>, ptr: &mut i32, i: i32, o: i32) -> Option<i32> {
-    println!("instruction {i} operand {o}");
+fn instr(r: &mut [u64; 3], p: &mut u64, i: u64, o: u64) -> Option<u64> {
+    let mut res = None;
     match i {
-        0 => {rs[0] = rs[0] / 2_i32.pow(combo(rs, o) as u32)}
-        1 => {rs[1] = rs[1] ^ o}
-        2 => {rs[1] = combo(rs,o) % 8}
-        3 => {if rs[0] != 0 {*ptr = o - 2}}
-        4 => {rs[1]  = rs[1] ^ rs[2]}
-        5 => {return Some(combo(rs, o) % 8) }
-        6 => {rs[1] = rs[0] / 2_i32.pow(combo(rs, o) as u32)}
-        7 => {rs[2] = rs[0] / 2_i32.pow(combo(rs, o) as u32)}
+        0 => {r[0] = r[0] / 2_u64.pow(c(r, o) as u32)}
+        1 => {r[1] = r[1] ^ o}
+        2 => {r[1] = c(r,o) % 8}
+        3 => {if r[0] != 0 {*p = o}}
+        4 => {r[1]  = r[1] ^ r[2]}
+        5 => {res =  Some(c(r, o) % 8) }
+        6 => {r[1] = r[0] / 2_u64.pow(c(r, o) as u32)}
+        7 => {r[2] = r[0] / 2_u64.pow(c(r, o) as u32)}
         _ => {panic!("invalid operand")}
     }
-    None
+    if i != 3 || r[0] == 0 {
+        *p += 2;
+    }
+    res
 }
 
 fn main() {
@@ -35,47 +39,131 @@ fn main() {
     let f = File::open(&args[1]).expect("couldn't open file");
     let mut r = BufReader::new(f);
 
-    let m = 8;
-    let mut rs: Vec<i32> = vec![0, 0, 0];
+    let mut reg_og: [u64;3] = [0;3];
     let mut s = String::new();
     for i in 0..3 {
         s.clear();
         r.read_line(&mut s).expect("couldn't read line");
         s = s.trim().to_string();
-        rs[i] = s
+        reg_og[i] = s
             .split(":")
             .skip(1)
-            .map(|n| n.trim().parse::<i32>().unwrap())
+            .map(|n| n.trim().parse::<u64>().unwrap())
             .collect::<Vec<_>>()[0];
     }
     s.clear();
     r.read_line(&mut s).expect("couldn't read line");
     r.read_line(&mut s).expect("couldn't read line");
-    s = s.split(":").skip(1).collect::<Vec<_>>()[0].trim().to_string();
-    let mut ins: Vec<i32> = s.split(",").map(|n| n.parse().unwrap()).collect();
-    let mut ptr :i32 = 0;
-    let mut v = Vec::<i32>::new();
+    s = s.split(":").skip(1).collect::<Vec<_>>()[0]
+        .trim()
+        .to_string();
+    let ins: Vec<u64> = s.split(",").map(|n| n.parse().unwrap()).collect();
+
+    let mut a =  0o133267275; // the digits in octal rep. of a don't change when it matches first few outputs
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    let mut best_i = 0;
     loop {
-        if ptr >= ins.len() as i32 {
+        let mut ptr: u64 = 0;
+        let mut found = true;
+        let mut i = 0;
+        let mut reg = reg_og.clone();
+        reg[0] = a;
+        loop {
+            if ptr as usize >= ins.len() {
+                break;
+            }
+            // println!("reg {:?}", reg);
+            // println!("ins {:?}", ins);
+            // println!("ptr {:?}", ptr);
+            let inst = ins[ptr as usize];
+            let op = ins[(ptr + 1) as usize];
+            let out = instr(&mut reg, &mut ptr, inst, op);
+            if !out.is_none() {
+                // println!("{}",out.unwrap());
+                if out.unwrap() != ins[i] {
+                    found = false;
+                    if i > best_i {
+                        best_i = i;
+                        let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+                        // println!("done {:.10}% s {:.2}", a as f64 / u64::MAX as f64, (end - start).as_secs());
+                        println!("{:o}: {}", a, i);
+                    }
+                    break;
+                }
+                i += 1;
+            }
+        }
+        if found && i == ins.len() {
             break;
         }
-        println!("reg {:?}", rs);
-        println!("ins {:?}", ins);
-        println!("ptr {:?}", ptr);
-        let inst = ins[ptr as usize];
-        let op = ins[(ptr+1) as usize];
-        let out = instr(&mut rs, &mut ptr, inst, op);
-        if !out.is_none() {
-            // print!("{},",out.unwrap())
-            v.push(out.unwrap());
-        }
-        ptr += 2;
+        a += 0o1000000000; // search the rest
     }
-    println!("reg {:?}", rs);
-    println!("ins {:?}", ins);
-    println!("ptr {:?}", ptr);
-    for n in v {
-        print!("{},",n);
-    }
-    println!();
+    println!("{}", a);
+    // for n in v {
+    //     print!("{},",n);
+    // }
+    // println!();
 }
+
+// attempt at a recursive solution
+// fn f_r(ins: &mut Vec<u64>, mut reg: [u64;3], ptr: u64, p_i: u64) -> u64 {
+//     let o = ins[ptr as usize +1];
+//     let i = ins[ptr as usize];
+//     match i {
+//         0 => {
+//             if reg[0] != u64::MAX {
+//                 if o == 4 {
+//                     if reg[0] != 0 {
+//                         return u64::MAX; // a_1 = floor(a_0 / 2^a_0) -> is always 0
+//                     }
+//                     reg[0] = u64::MAX; // could have been any value
+//                 }
+//                 let val = c(&reg, o);
+//                 if val == u64::MAX {
+//                     reg[(o - 4) as usize] = 0; // explore all values?
+//                 }
+//                 reg[0] = reg[0] * 1_u64 << c(&reg, o);
+//             }
+//         }
+//         1 => {
+//             if reg[1] != u64::MAX {
+//                 if o == 5 {
+//                     if reg[1] != 0 {
+//                         return u64::MAX; // a_1 = a_1 ^ a_1 -> is always 0
+//                     }
+//                     reg[1] = u64::MAX; // could have been any value
+//                 }
+//                 let val = c(&reg, o);
+//                 if val == u64::MAX {
+//                     reg[(o - 4) as usize] = 0; // explore all values?
+//                 }
+//                 reg[1] = reg[1] ^ c(&reg, o);
+//             }
+//         }
+//         5 => {
+//             // printing the next value
+//             let to_print = ins[p_i as usize];
+//             let val = c(&reg, o);
+//             if val == u64::MAX{
+//                 reg[(o - 4) as usize] = to_print;
+//             }
+//             if val % 8 != to_print {
+//                 return u64::MAX;
+//             }
+//             if p_i == 0 {
+//                 return reg[0];
+//             }
+//         }
+//         _ => {}
+//     }
+//     // correct configuration for printing, a previous step could be jumped, or one-step
+//     // find places where we could jump to this instruction
+//     let mut best = f_r(ins, reg, ptr - 2, p_i -1);
+//     for i in (0..ins.len()).step_by(2) {
+//         if ins[i] == 3 && ins[i + 1] == ptr {
+//             best = best.min(f_r(ins, reg, i as u64, p_i-1));
+//         }
+//     }
+//     best
+// }
+

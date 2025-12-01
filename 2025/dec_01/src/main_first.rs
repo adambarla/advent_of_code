a2ef51309788ee4e1b9b173ef30c0b28068d08db
Adam Barla <adam.barla@epfl.ch>
2025-12-01 23:17:32 +0100

    feat(2025): 01 **

--- 2025/dec_01/src/main_first.rs
+++ 2025/dec_01/src/main_first.rs
@@ -10,32 +10,28 @@ fn main() {
     let file_name: &String = &args[1];
     let input = fs::read_to_string(file_name).unwrap();
 
-    let n = 100;
-    let mut pos: i32 = 50;
+    let mut pos = 50;
     let mut pass = 0;
     for line in input.split("\n") {
+        // println!("{}", line);
         let dir = match line.chars().nth(0) {
             Some(d) => d,
             None => break
         };
         let num = &line[1..].parse::<i32>().unwrap();
-        let mut step = 1;
         if dir == 'L' {
-            step = -1;
+            pos = (pos - num + 100) % 100;
+        } 
+        else if dir == 'R' {
+            pos = (pos + num + 100) % 100;
         }
-        for i in 0..num.abs() {
-            let prev = pos;
-            pos += step;
-            if pos < 0 {
-                pos += n;
-            }
-            if pos >= n {
-                pos -= n;
-            }
-            if pos == 0 {
-                pass += 1;
-            }
+        else {
+            panic!("unknown direction")
         }
+        if pos == 0 {
+            pass += 1;
+        }
+        // println!("{}", pos)
     }
     println!("{}", pass);
 }
